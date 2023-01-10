6b484d091c15501c4903cd2fd01cc5c7c5feedc2
Renan Romagnoli <renanromagnoli@yahoo.com.br>
2023-01-10 20:36:56 -0300

    class Store created

---
+++ src/core/store.ts
@@ -0,0 +1,32 @@
+export default class Store {
+    #id: number
+    #name: string
+    #thumbnail: string
+    #link: string
+    #hasOffer: number
+
+    constructor(id: number, name: string, thumbnail:string, link:string, hasOffer:number) {
+        this.#id = id
+        this.#name = name
+        this.#thumbnail = thumbnail
+        this.#link = link
+        this.#hasOffer = hasOffer
+    }
+
+    get id() {
+        return this.#id
+    }
+    get name() {
+        return this.#name
+    }
+    get thumbnail() {
+        return this.#thumbnail
+    }
+    get link() {
+        return this.#link
+    }
+    get hasOffer() {
+        return this.#hasOffer
+    }
+
+}
