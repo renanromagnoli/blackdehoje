c3424c8a58432690d3e5f8e66f57ac254cdbb0d3
Renan Romagnoli <renanromagnoli@yahoo.com.br>
2023-01-10 20:26:44 -0300

    server dev structure created

--- src/service/Api.ts
+++ src/service/Api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default {
     getCategoryOffers: async (category: number) => {
-        const url = `http://sandbox-api.lomadee.com/v3/${process.env.TOKEN}/offer/_category/${category}?sourceId=${process.env.SOURCE_ID}`
+        const url = `${process.env.LMDURL}/${process.env.TOKEN}/offer/_category/${category}?sourceId=${process.env.SOURCE_ID}`
         const {data} = await axios.get(url)
         return data
     }
