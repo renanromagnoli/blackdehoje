857f3bd4db2602b4941d30de30160308841c3eb2
Renan Romagnoli <renanromagnoli@yahoo.com.br>
2023-01-12 20:05:18 -0300

    item links added

--- src/components/Item/index.tsx
+++ src/components/Item/index.tsx
@@ -26,18 +26,24 @@ export function Item(props: ItemProps){
         <div className={styles.itemCard}>
             <Link href={`https://api.whatsapp.com/send?text=${props.link}`} legacyBehavior>
                 <a target={'_blank'}>
-                    <div className={styles.shareItem} title='Compartilhar'><ShareIcon fontSize='medium' style={{color: 'black'}} /></div>
+                    <div className={styles.shareItem} title='Compartilhar no Whatsapp'><ShareIcon fontSize='medium' style={{color: 'green'}} /></div>
                 </a>
             </Link>
-            <div className={styles.itemContent}>
-                <img src={props.thumbnail} alt="image" />
-                <p className={styles.name}>{props.name}</p>
-                <div className={styles.itemFooter}>
-                    {props.oldPrice == 0 ? '' : <div className={styles.oldPrice}>{props.oldPrice}</div> }
-                    <div className={styles.price}>{reais}</div>
-                    <img className={styles.storeThumb} src={props.storeThumb} alt="Store Thumbnail" />
-                </div>
-            </div>
+            <Link href={props.link}  legacyBehavior>
+                <a className={styles.link} target="_blank"  rel="noopener noreferrer">
+                    <div className={styles.itemContent}>
+                            <img src={props.thumbnail} alt="image" title='Acessar produto na loja'/>
+                            <p className={styles.name} title='Acessar produto na loja'>{props.name}</p>
+                            <div className={styles.itemFooter}>
+                                {props.oldPrice == 0 ? '' : <div className={styles.oldPrice}>{props.oldPrice}</div> }
+                                <div className={styles.price} title='Acessar produto na loja'>{reais}</div>
+                                <a href="#" target='_blank' title='Filtrar por produtos dessa loja'>
+                                    <img className={styles.storeThumb} src={props.storeThumb} alt="Store Thumbnail" />
+                                </a>
+                            </div>
+                    </div>
+                </a>
+            </Link>
         </div>
     )
 }
