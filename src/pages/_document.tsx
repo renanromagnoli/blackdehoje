4bd60fa6131dec24f44aa73b030c3a4b78f45bd5
Renan Romagnoli <renanromagnoli@yahoo.com.br>
2023-01-17 20:02:15 -0300

    Title changed

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,7 @@ export default function Document() {
         <meta name="description" content="Central de descontos e promoções." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
-        <title>Black de Hoje</title>
+        <title>Black Hoje</title>
         
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin=''/>
