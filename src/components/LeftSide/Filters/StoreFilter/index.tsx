b2b85d2836da30fc75a9d21f3c402df93d463948
Renan Romagnoli <renanromagnoli@yahoo.com.br>
2023-01-25 03:05:27 -0300

    feat: Added remove Store function

--- src/components/LeftSide/Filters/StoreFilter/index.tsx
+++ src/components/LeftSide/Filters/StoreFilter/index.tsx
@@ -1,3 +1,5 @@
+import { useContext } from 'react'
+import { StoresContext } from '../../../../contexts/StoresContext'
 import StoreModel from '../../../../core/store'
 import styles from './styles.module.scss'
 
@@ -6,9 +8,18 @@ interface StoreFilterProps {
 }
 
 export function StoreFilter({store}: StoreFilterProps) {
+
+    const {listSelectedStores, setListSelectedStores} = useContext(StoresContext)
+
+    function removeStore(store) {
+        let index = listSelectedStores.indexOf(store)
+        listSelectedStores.splice(index, 1)
+        setListSelectedStores([...listSelectedStores])
+    }
+
     return (
         <div className={styles.storesFilterContent}>
-            <div className={styles.button}>X</div>
+            <div className={styles.button} onClick={() => removeStore(store)}>X</div>
             <img src={store.thumbnail} alt={store.name} />
         </div>
     )
