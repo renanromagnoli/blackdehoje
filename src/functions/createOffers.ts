b89f83665e0cf6694c734564c461e5b5d475e306
Renan Romagnoli <renanromagnoli@yahoo.com.br>
2023-02-03 15:55:00 -0300

    feat: Create function upCategoryOffersContext

--- src/functions/createOffers.ts
+++ src/functions/createOffers.ts
@@ -1,9 +1,14 @@
+import { ReactNode } from 'react'
 import { CategoryModel } from '../core/category'
 import OfferModel from '../core/offer'
 import Api from '../service/Api'
 
+function setRandomNumberBetween(n1:number, n2:number) {
+  return Math.floor(Math.random() * (n2 - n1) + n1)
+}
 
-export async function createOffersModel(offers): Promise<OfferModel[]> {
+
+export function createOffersModel(offers): Promise<OfferModel[]> {
     return offers.map((offer: any) => {
       const model = new OfferModel(offer.id, offer.name, offer.category, offer.link, offer.thumbnail, offer.price, offer.discount, offer.store)
       if(offer.priceFrom) model.priceFrom = offer.priceFrom
@@ -11,34 +16,62 @@ export async function createOffersModel(offers): Promise<OfferModel[]> {
     })
 }
 
-export async function createCategoryOffer(category: CategoryModel, page=1) {
-  let categoryOffers = await Api.getCategoryOffers(category.id, page)
-  if(categoryOffers) {
+export async function createCategoryOffer(category: CategoryModel, page: number) {
+  let data = await Api.getCategoryOffers(category.id, page)
+  if(data) {
+    const modelOffers = createOffersModel(data.offers)
     return {
-      [category.name]: {
-        show: true,
-        pagination: {page: categoryOffers.pagination.page, dateReq: new Date(), offers: categoryOffers.offers},
-        totalPages: categoryOffers.pagination.totalPage
-      }
+      dateReq: new Date(),
+      offers: modelOffers,
+      page,
+      totalPage: data.pagination.totalPage
     }
   }
 }
 
-export async function createCategoriesOffers(categories: CategoryModel[], categoriesOffers: OfferModel[]){
-  categories?.map(async category => {
-      let req = await Api.getCategoryOffers(category.id)
-      if(req)
-      // offers.push(offer)
-      return {
-        ...categoriesOffers,
+async function createCategoryOfferWithRandomPage(category: CategoryModel, totalPage: number) {
+  if(totalPage > 1) {
+    const totalPages = totalPage
+    const randomPage = setRandomNumberBetween(1, totalPages)
+    return await createCategoryOffer(category, randomPage)
+  } else {
+    return await createCategoryOffer(category, 1)    
+  }
+}
+
+function filterLimitTime(offers, limitMinutesTime=15) {
+  let dateNow = new Date()
+  offers.forEach((offer, i) => {
+    let reqOfferMinutes = offer?.dataReq.getMinutes()
+    if((dateNow.getMinutes() - reqOfferMinutes) > limitMinutesTime) offers.splice(i, 1)
+  })
+  return offers
+}
+
+export async function upCategoryOffersContext(category: CategoryModel, categoriesOffersContext: any){
+  
+  const categoryOffersCtxt = categoriesOffersContext
+  const offersPage = await createCategoryOfferWithRandomPage(category, categoryOffersCtxt[category.name].totalPages)
+
+  // const offersTimeReview = filterLimitTime(categoryOffersCtxt[category.name].offersPage)
+
+  if(offersPage) {
+    return [
+      ...categoryOffersCtxt,
+      {
         [category.name]: {
           show: true,
-          pagination: {page: req.pagination.page, dateReq: new Date(), offers: req.offers},
-          totalPages: req.pagination.totalPage
+          offersPage: {
+            // ...offersTimeReview,
+            [offersPage.page]: {
+              dateReq: offersPage.dateReq, 
+              offers: offersPage.offers
+            }
+          }
+          ,
+          totalPages: offersPage.totalPage
         }
       }
-  })
-  // console.log('offers: ', offers)
-  // return offers
-  
+    ]  
+  }
 }
